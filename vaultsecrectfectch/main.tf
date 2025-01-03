7b017cb237403d98dd55e69e214483f2bd367e69
kp3073 <keyurpatel8288@gmail.com>
2025-01-03 12:14:04 -0500

    ansible terraform | vault output testing

--- vaultsecrectfectch/main.tf
+++ vaultsecrectfectch/main.tf
@@ -8,6 +8,8 @@ variable "token" {}
 data "vault_kv_secret" "secret_data" {
   path = "test/data/dem-ssh"
 }
-output "secret_data" {
-  value = data.vault_kv_secret.secret_data.data
+
+resource "local_file" "secret" {
+  filename = "/tmp/secret"
+  content = data.vault_kv_secret.secret_data.data["password"]
 }
