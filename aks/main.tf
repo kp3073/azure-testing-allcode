905b21e9eb5bc16051e117b1efa16dabb5c6dcb0
kp3073 <keyurpatel8288@gmail.com>
2025-01-09 12:43:16 -0500

    terraform aks

---
+++ aks/main.tf
@@ -0,0 +1,45 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2"
+}
+
+data "azurerm_resource_group" "main" {
+  name = "azuredevops"
+}
+
+data "azurerm_subnet" "main" {
+  name                 = "default"
+  virtual_network_name = "azure-network"
+  resource_group_name  = data.azurerm_resource_group.main.name
+}
+
+
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "example-aks1"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  dns_prefix          = "cluster"
+
+  default_node_pool {
+	name       = "default"
+	node_count = 1
+	vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+	type = "SystemAssigned" 
+	
+  }
+
+  tags = {
+	Environment = "Production"
+  }
+}
+
+
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.main.kube_config_raw
+  sensitive = true
+}
