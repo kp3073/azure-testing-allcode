129a851d794c217d6f40afd1e5dfefb0dd916aeb
kp3073 <keyurpatel8288@gmail.com>
2025-02-14 12:58:51 -0500

    Helm dev-destroy added

--- terraform import/main.tf
+++ terraform import/main.tf
@@ -2,28 +2,33 @@ provider "azurerm" {
   features {}
   subscription_id = "4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2"
 }
+# 
+# resource "azurerm_linux_virtual_machine" "example" {
+#   name                = "workstation"
+#   location            = "UK West"
+#   resource_group_name = "AZUREDEVOPS"
+#   size                = "Standard_D2s_v3"
+#   admin_username      = "centos"
+#   admin_password      = "Adminadmin1234$"
+# 
+#   priority        = "Spot"
+#   eviction_policy = "Deallocate"
+#   max_bid_price   = -1  # Pay up to the standard VM price
+# 
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
+# 
+#   network_interface_ids = ["<NetworkInterfaceID>"]
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "18.04-LTS"
+#     version   = "latest"
+#   }
+# }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "vault"
-  location            = "ukwest"
-  resource_group_name = "azuredevops"
-  size                = "Standard_DS1_v2"
-  admin_username      = "vault"
-  admin_password      = "Adminadmin123$"
 
-  priority        = "Spot"
-  eviction_policy = "Deallocate"
-  max_bid_price   = -1  # Pay up to the standard VM price
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  network_interface_ids = ["<NetworkInterfaceID>"]
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+resource "azurerm_virtual_machine" "existing_vm" {
+  # Placeholder for the VM configuration
 }
