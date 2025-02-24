162aff0dc1865f59ce1a751f3d358e372bd40e60
kp3073 <keyurpatel8288@gmail.com>
2025-02-24 13:42:14 -0500

    tools-automation - change instance type

--- terraform import/main.tf
+++ terraform import/main.tf
@@ -1,65 +1,43 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2"
-}
-
-# main.tf
-data "azurerm_resource_group" "main" {
-name = "azuredevops"
-}
-
-
-data "azurerm_subnet" "main" {
-name = "default"
-resource_group_name = data.azurerm_resource_group.main.name
-virtual_network_name = "azure-network"
-}
-
-
-
-
-
-# Network Interface for the new Spot VM
 resource "azurerm_network_interface" "main" {
-name = "my-new-nic"  # Change the NIC name
-location = data.azurerm_resource_group.main.location
-resource_group_name = data.azurerm_resource_group.main.name
+  name                = hc-nic
+  location            = "West Uk"
+  resource_group_name = "azuredevops"
 
-ip_configuration {
-name = "internal"
-subnet_id = "/subscriptions/4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2/resourceGroups/azuredevops/providers/Microsoft.Network/virtualNetworks/azure-network/subnets/default"  # Replace with your subnet ID
-private_ip_address_allocation = "Dynamic"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
 }
-}
+resource "azurerm_virtual_machine" "spot_vm" {
+  name                = "hcl2"
+  location            = "West Uk"
+  resource_group_name = "azuredevops"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size             = "Standard_D2s_v3"
 
-# New Spot VM
-resource "azurerm_linux_virtual_machine" "main" {
-name = "test-vm"  # Change the VM name
-location = data.azurerm_resource_group.main.location
-resource_group_name = data.azurerm_resource_group.main.name
-size = "Standard_DS1_v2"  # Replace with your desired VM size
-admin_username = "centos"        # Replace with your admin username
-network_interface_ids = [azurerm_network_interface.main.id]
+  priority        = "Spot"
+  eviction_policy = "Deallocate"
 
-  admin_ssh_key {
-    username = "adminuser"
-    public_key = file("/Users/keyurpatel/Library/CloudStorage/OneDrive-AlignTechnology,Inc/Documents/id_rsa.pub")
+  os_profile {
+	computer_name  = "MySpotVM"
+	admin_username = "adminuser"
+	admin_password = "P@ssword123!"
   }
 
-os_disk {
-name = "my-new-spot-vm-osdisk"  # Change the OS disk name
-caching = "ReadWrite"
-storage_account_type = "Standard_LRS"
-}
-  
-source_image_reference {
-  publisher = "Canonical"
-  offer     = "UbuntuServer"
-  sku       = "18.04-LTS"
-  version   = "latest"
-}
+  os_profile_linux_config {
+	disable_password_authentication = false
+  }
 
-priority = "Spot"  # This makes it a Spot VM
-eviction_policy = "Deallocate"  # Set the eviction policy (optional)
-}
+  storage_os_disk {
+	name              = "MySpotVM_OSDisk"
+	caching           = "ReadWrite"
+	create_option     = "FromImage"
+	managed_disk_type = "Premium_LRS"
+  }
 
+  source_image_reference {
+	id = "/subscriptions/4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2/resourceGroups/azuredevops/providers/Microsoft.Network/networkInterfaces/hcl231"
+  }
+}
